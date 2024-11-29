1c6d1c5cf76e3710f50d0b1af04ed8546ca6ed47
lewiEyasu <eyasulewi@gmail.com>
2024-11-29 22:55:36 +0300

    Add random salutation function to para2pdf.py
    - Introduced `random_salutation()` function that returns a random salutation from a predefined list.
    - This addition enhances the script's capabilities for generating personalized PDF content.

--- para2pdf.py
+++ para2pdf.py
@@ -6,7 +6,14 @@ def today_date() -> str:
     Get today's date in the format of "Month Day, Year"
     """
     today = datetime.now()
-
     formatted_date = today.strftime("%B %d, %Y")
-
     return formatted_date
+
+def random_salutation() -> str:
+   """
+   Get a random salutation
+   """      
+   temp = ["Sincerely,", "Regards,", "Best,", "Respectfully,", "Thank you,", "Thank you for your consideration,"]
+   return (random.choice(temp))    
+
+
